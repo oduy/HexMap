f5f7aab9069aef97ec5e0adbde064e41469cffd6
Ô Duy <33828921+oduy@users.noreply.github.com>
2019-07-30 01:40:18 +0700

    add git ignore

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefabs;
+    HexCell[] cells;
+
+    public Text cellLabelPrefab;
+    Canvas gridCanvas;
+
+    HexMesh hexMesh;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        cells = new HexCell[height * width];
+        for( int z = 0, i = 0; z < height; z++ )
+        {
+            for(int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    private void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMatrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMatrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefabs);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+    
+}
